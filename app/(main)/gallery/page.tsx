83978f86f6b17f21ccd42ae9ed395176a55113fa
Zen9r <muhammedaljarba8@gmailcom>
2025-07-11 18:25:34 +0300

    تحسين في المعرض

--- app/(main)/gallery/page.tsx
+++ app/(main)/gallery/page.tsx
@@ -139,7 +139,7 @@ export default function GalleryPage() {
       <AnimatePresence>
   {selectedImage && (
     <motion.div
-      className="fixed inset-0 bg-black/80 flex items-center justify-center z-50 p-4"
+      className="fixed inset-0 bg-black/80 flex items-center justify-center z-[999] p-4" // <--- تم التعديل هنا
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
